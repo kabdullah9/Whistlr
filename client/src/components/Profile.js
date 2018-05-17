2d44a37183f3a97d2d9a5aabd0186629487a5a54
davidlatuno <dslatuno@gmail.com>
2018-05-17 13:40:19 -0700

    took modal away from login and added modal to signup

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,14 +6,12 @@ import { Link } from 'react-router-dom';
 class Profile extends Component {
 
   state = {
-    username: "",
     email: ""
   };
 
   componentDidMount() {
     API.getUser(this.props.user.id).then(res => {
       this.setState({
-        username: res.data.username,
         email: res.data.email
       })
     });
@@ -23,7 +21,6 @@ class Profile extends Component {
     return (
       <div className="container Profile">
         <h1>On the profile page!</h1>
-        <p>Username: {this.state.username}</p>
         <p>Email: {this.state.email}</p>
         <Link to="/">Go home</Link>
       </div>
